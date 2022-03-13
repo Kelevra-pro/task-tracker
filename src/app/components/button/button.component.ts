3fb40d8b7c2b8ac2572b49c3d05598663f0a64e0
kelevra-pro <damtat8@gmail.com>
2022-03-13 23:04:01 +0300

    component input data

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
-  styleUrls: ['./button.component.css']
+  styleUrls: ['./button.component.css'],
 })
 export class ButtonComponent implements OnInit {
+  @Input() text: string;
+  @Input() color: string;
+  @Output() btnClick = new EventEmitter();
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  onClick() {
+    this.btnClick.emit();
+  }
 }
