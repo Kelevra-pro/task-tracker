8b79aeac4f5b7c14e749d3a020c7a16fc8da7919
kelevra-pro <damtat8@gmail.com>
2022-04-04 14:21:37 +0300

    toggle button and ui service

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { TaskService } from '../../sevices/task.service';
+import { TaskService } from '../../services/task.service';
 import { Task } from '../../Task';
 
 @Component({
